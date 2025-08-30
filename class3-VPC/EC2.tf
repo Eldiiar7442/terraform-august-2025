258a33822d0851043fdd2cfb125f9ae5aeec2e65
EC2 Default User <ec2-user@ip-172-31-26-60.eu-west-2.compute.internal>
2025-08-30 08:21:04 +0000

    folder

---
+++ class3-VPC/EC2.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ubuntu.id # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+  associate_public_ip_address = true 
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = aws_subnet.main1.id
+  user_data = file("apache.sh") 
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-eldi-key"                    # имя ключа в AWS
+  public_key = file("/home/ec2-user/.ssh/id_ed25519.pub")  # полный путь к публичному ключу на этом EC2
+}
