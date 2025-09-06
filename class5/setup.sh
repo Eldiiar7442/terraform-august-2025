2d66683fb4e5f3b15cf0fe663b17c838d6482d1b
Eldiiar7442 <rayymbekuulu.e@gmail.com>
2025-09-06 06:18:56 +0000

    111

--- class5/setup.sh
+++ class5/setup.sh
@@ -4,3 +4,7 @@ sudo yum install httpd -y
 sudo systemctl start httpd 
 sudo systemctl enable httpd 
 sudo yum install tree -y
+
+wget https://releases.hashicorp.com/terraform/1.13.1/terraform_1.13.1_linux_amd64.zip
+unzip terraform_1.13.1_linux_amd64.zip
+sudo mv terraform /usr/locol/bin/
